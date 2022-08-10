721f0bf62279acf8987fd861be158765110fb75f
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-10 09:36:26 -0500

    Render de choices

--- src/app.js
+++ src/app.js
@@ -3,12 +3,27 @@ import questions from "./data/questions.js";
 import Quiz from "./models/Quiz.js";
 import UI from "./models/UI.js";
 
+/**
+ * @param {Quiz} quiz Questions, choices and answers of the quiz
+ * @param {UI} ui	Quiz to render
+ */
+function renderPage(quiz, ui) {
+  if (quiz.isEnded()) {
+    ui.showScore(quiz.score);
+  } else {
+    ui.showQuestion(quiz.getQuestionsIndex().text);
+    ui.showChoices(quiz.getQuestionsIndex().choices, (currentChoice) => {
+      quiz.guess(currentChoice);
+      renderPage(quiz, ui);
+    });
+  }
+}
+
 function main() {
   const quiz = new Quiz(questions);
   const ui = new UI();
 
-  ui.showQuestion(quiz.getQuestionsIndex().text);
-  ui.showChoices(quiz.getQuestionsIndex().choices);
+  renderPage(quiz, ui);
 }
 
 main();
--- src/models/UI.js
+++ src/models/UI.js
@@ -9,11 +9,37 @@ export default class UI {
   }
   /**
    * @param {string[]} choices Choices of the question to render
+   * @param {function} callBack answer selected
    */
-  showChoices(choices) {
+  showChoices(choices, callBack) {
     const choicesList = document.getElementById("choices");
-    choicesList.innerHTML = choices
-      .map((choice) => `<button class="btn">${choice}</button>`)
-      .join("");
+    choicesList.innerHTML = "";
+
+    for (let i = 0; i < choices.length; i++) {
+      const button = document.createElement("button");
+      button.className = "btn";
+      button.textContent = choices[i];
+      button.addEventListener("click", () => callBack(choices[i]));
+
+      choicesList.appendChild(button);
+    }
+  }
+  /**
+   * @param {number} score Total score
+   */
+  showScore(score) {
+    const quizEndHTML = `
+    <h1>Result</h1>
+    <h2>Your Score: ${score}</h2>
+    `;
+    const element = document.getElementById("quiz");
+    element.innerHTML = quizEndHTML;
+  }
+  /**
+   * @param {number} currentIndex Current index of the quiz
+   * @param {number} total Total questions
+   */
+  showProgress(currentIndex, total) {
+    document.getElementById("progress").innerHTML =;
   }
 }
